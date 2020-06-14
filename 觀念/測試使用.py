83be32ba504353311208f6c4142e24c9ad362953
silencejamie <as124122323@gmail.com>
2020-06-14 12:50:52 +0800

    Line bot by django

--- 觀念/測試使用.py
+++ 觀念/測試使用.py
@@ -1,20 +1,23 @@
-di = {
-    'name': 'jamie',
-    'age': 18
-}
-def di_print(name, age):
-    print(name, age)
-di_print(**di)  # **di => 參數會變成 name=jamie和age=18(要使用的話字典的key要與函數的參數名相同)
+# di = {
+#     'name': 'jamie',
+#     'age': 18
+# }
+# def di_print(name, age):
+#     print(name, age)
+# di_print(**di)  # **di => 參數會變成 name=jamie和age=18(要使用的話字典的key要與函數的參數名相同)
 
 
-
-
-import math
-
-print(math.log(100, 5))
-5*(5*2)
+# import math
+#
+# print(math.log(100, 5))
+# 5*(5*2)
 
 
 # --------篩選值-----------
-li = None or [] or [1, 2]
-print(li)
+# li = None or [] or [1, 2]
+# print(li)
+
+
+# --------切片-----------
+li = [1, 2, 3, 4]
+print(li[1:-1])
